b28461a270cd6b5937878504622cc6cdf1b41780
Espen Solhaug <espen.solhaug@no.experis.com>
2024-11-28 11:07:22 +0100

    barebone goalcomponent

---
+++ src/Components/GoalComponent/GoalComponent.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "../../App.css";
+import "./goalComponent.css";
+
+const goalComponent = () => {
+  return <div className="goalComponent"></div>;
+};
+
+export default goalComponent;
